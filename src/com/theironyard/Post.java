460bbf863cf78a53e530ae3c6efad1ab67803a81
willriggins <williamriggins2013@gmail.com>
2016-05-26 10:53:35 -0400

    stuff and things

---
+++ src/com/theironyard/Post.java
@@ -0,0 +1,16 @@
+package com.theironyard;
+
+/**
+ * Created by will on 5/26/16.
+ */
+public class Post {
+    int replyId;
+    String author;
+    String text;
+
+    public Post(int replyId, String author, String text) {
+        this.replyId = replyId;
+        this.author = author;
+        this.text = text;
+    }
+}
